e492755b1fac514ed0bcbdc3bd2888092e738543
Danilo Martinelli <danilomartinelli429@gmail.com>
2023-12-21 17:36:29 -0300

    fix: tests

--- tests/unit/test_answer_service.py
+++ tests/unit/test_answer_service.py
@@ -30,27 +30,26 @@ class TestAnswerService(unittest.TestCase):
         self.assertTrue(is_correct)
         self.assertEqual(new_score, 10)
 
+    def test_process_answer_incorrect(self):
+        question_id = "question-1"
+        user_id = "user-1"
+        correct_answer = "answer-1"
+        question_data = {
+            "_id": question_id,
+            "correctAnswer": correct_answer
+        }
 
-def test_process_answer_incorrect(self):
-    question_id = "question-1"
-    user_id = "user-1"
-    correct_answer = "answer-1"
-    question_data = {
-        "_id": question_id,
-        "correctAnswer": correct_answer
-    }
+        self.question_repo.get_question_by_id.return_value = question_data
 
-    self.question_repo.get_question_by_id.return_value = question_data
+        initial_score = 0
+        self.user_repo.get_user_by_id.return_value = {"_id": user_id, "score": initial_score}
 
-    initial_score = 0
-    self.user_repo.get_user_by_id.return_value = {"_id": user_id, "score": initial_score}
+        user_answer = "incorrect-answer"
 
-    user_answer = "incorrect-answer"
+        is_correct, new_score = self.question_service.process_answer(question_id, user_id, user_answer)
 
-    is_correct, new_score = self.question_service.process_answer(question_id, user_id, user_answer)
-
-    self.assertFalse(is_correct)
-    self.assertEqual(new_score, initial_score)
+        self.assertFalse(is_correct)
+        self.assertEqual(new_score, initial_score)
 
 
 if __name__ == "__main__":
